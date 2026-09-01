90103ced349032d196f81d54c20c5f7451200909
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 14:58:39 +0100

    grading system

---
+++ level2_Grading_System.js
@@ -0,0 +1,14 @@
+let score = 80
+
+if (score >= 90 && score <= 100){
+    console.log("A");
+    
+}
+else if (score >= 80 && score <= 89){
+
+    console.log("B")
+}
+else if (score >= 70 && score <= 79){
+
+    console.log("C")
+}else console.log("F")
