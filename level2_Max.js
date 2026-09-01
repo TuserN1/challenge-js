3d1e994eda4c23a38a0bf23d4c16a54956c23e23
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 15:05:06 +0100

    max of three

---
+++ level2_Max.js
@@ -0,0 +1,13 @@
+let a = 2
+let b = 3
+let c = 7
+
+if (a > b && a > c){
+    console.log(a + " is the largest")
+}
+else if (c >b && c > a){
+    console.log(c + " is the largest")
+}
+if (b >a && b > c){
+    console.log(b + " is the largest")
+}
