9ec1ef16ad4d1a1f3453c5de2f6c967133c48b64
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 15:57:12 +0100

    are you a triangles !??

---
+++ level3_Valid_Triangle.js
@@ -0,0 +1,12 @@
+let angle1 = 80
+let angle2 = 80
+let angle3 = 80
+let angles ;
+if (angle1 > 0 && angle2 > 0 && angle3 > 0){
+    angles = angle1 + angle2 + angle3
+    if (angles == 180) {
+        console.log("this is a triangle");
+        
+    }else console.log("this is not a triangle");
+
+}
