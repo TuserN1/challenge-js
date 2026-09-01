488239ff65f22a521a094cf333b5bc199292c166
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 16:50:11 +0100

    bmi

---
+++ level3_BMI_calcul.js
@@ -0,0 +1,12 @@
+let weight = 60
+let height = 180
+
+if ((weight / height * height) > 25){
+    console.log("Overweight")
+}
+else if ((weight / height * height) >= 18,5){
+    console.log("normal")
+}
+if ((weight / height * height) < 18.5 ){
+    console.log("Underweight")
+}
